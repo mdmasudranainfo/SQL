7e2fd69d61f844a62a27e1e79372e8abd95e9b56
mdmasudranainfo <mdmasude7@gmail.com>
2023-08-10 10:37:09 +0600

    first commit

---
+++ opration.sql
@@ -0,0 +1,21 @@
+-- CREATE DATABASE masud;
+
+
+-- CREATE Table alluser(
+--     user_id SERIAL PRIMARY KEY UNIQUE,
+--     userName VARCHAR(255) UNIQUE NOT NULL,
+--     email VARCHAR(255) UNIQUE NOT NULL
+-- )
+
+
+
+
+-- ##insert data
+
+-- INSERT INTO alluser VALUES(1,'this', 'abc@gmail.com')
+
+
+
+
+-- ##get all user B
+select * from alluser;
